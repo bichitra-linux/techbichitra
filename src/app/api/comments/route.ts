4800da2b4cac008b0fa4b656e43405637d4a8f81
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-01 14:26:03 +0545

    Refactor GET function in route.ts to use TypeScript for req parameter and handle missing URL

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -3,7 +3,7 @@ import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
 // GET ALL COMMENTS OF A POST
-export const GET = async (req) => {
+export const GET = async (req: Request) => {
   const { searchParams } = new URL(req.url);
 
   const postSlug = searchParams.get("postSlug");
@@ -16,22 +16,20 @@ export const GET = async (req) => {
       include: { user: true },
     });
 
-    return new NextResponse(JSON.stringify(comments, { status: 200 }));
+    return new NextResponse(JSON.stringify(comments), { status: 200 });
   } catch (err) {
     // console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-    );
+    return new NextResponse(JSON.stringify({ message: "Something went wrong!" }), { status: 500 });
   }
 };
 
 // CREATE A COMMENT
-export const POST = async (req) => {
+export const POST = async (req: Request) => {
   const session = await getAuthSession();
 
-  if (!session) {
+  if (!session || !session.user) {
     return new NextResponse(
-      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+      JSON.stringify({ message: "Not Authenticated!" }), { status: 401 }
     );
   }
 
@@ -41,11 +39,11 @@ export const POST = async (req) => {
       data: { ...body, userEmail: session.user.email },
     });
 
-    return new NextResponse(JSON.stringify(comment, { status: 200 }));
+    return new NextResponse(JSON.stringify(comment), { status: 200 });
   } catch (err) {
     console.log(err);
     return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+      JSON.stringify({ message: "Something went wrong!" }), { status: 500 }
     );
   }
 };
