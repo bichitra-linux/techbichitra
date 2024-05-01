fb25de9a682bf4098b69db64ea036d188215be33
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-01 11:31:21 +0545

    Update layout.tsx to include Navbar, Footer, ThemeContextProvider, ThemeProvider, and AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,36 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
+import AuthProvider from "@/providers/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "TechBichitra",
-  description: "Everything about tech",
+  description: "Everything about Tech",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
+              </div>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
