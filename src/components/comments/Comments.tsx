de533897b086c7a325576a8c7b4d806f5620cb3a
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-01 11:40:47 +0545

    Update styles import in Comments component

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import styles from "./comments.module.css";
+import styles from "./comments.module.scss";
 import Image from "next/image";
 import useSWR from "swr";
 import { useSession } from "next-auth/react";
