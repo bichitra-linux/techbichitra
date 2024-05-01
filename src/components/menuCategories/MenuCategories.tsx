a5af0c30aecdac2a52d40205429eb5a8cdf04fa0
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-01 11:40:55 +0545

    Update styles import in MenuCategories component

--- src/components/menuCategories/MenuCategories.tsx
+++ src/components/menuCategories/MenuCategories.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import styles from "./menuCategories.module.css";
+import styles from "./menuCategories.module.scss";
 
 const MenuCategories = () => {
   return (
