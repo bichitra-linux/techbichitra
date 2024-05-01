61ff18eef8e4db782be189a545a5216b5bec324b
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-01 13:46:07 +0545

    Refactor Home component to use TypeScript for searchParams prop in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,24 @@ import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
 
-export default function Home({ searchParams }) {
-  const page = parseInt(searchParams.page) || 1;
+interface HomeProps {
+  searchParams: {
+    page: string;
+    cat: string; // cat is now optional
 
+    // other properties...
+  };
+}
+
+export default function Home({ searchParams }: HomeProps) {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
   return (
     <div className={styles.container}>
       <Featured />
       <CategoryList />
       <div className={styles.content}>
-        <CardList page={page}/>
+        <CardList page={page} cat={cat}/>
         <Menu />
       </div>
     </div>
