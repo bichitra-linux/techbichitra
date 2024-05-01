e5534434b30c665ce75e9292783081a84bc0d22b
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-01 13:46:39 +0545

    Refactor GET function in route.ts to use TypeScript for req parameter and handle missing URL

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,10 +2,15 @@ import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-export const GET = async (req) => {
-  const { searchParams } = new URL(req.url);
+export const GET = async (req: { url: string | null }) => {
+  if (req.url === null) {
+    const errorMessage = "URL is missing!";
+    return new NextResponse(JSON.stringify({ message: errorMessage }), { status: 400 });
+  }
 
-  const page = searchParams.get("page");
+  const { searchParams } = new URL(req.url as string);
+
+  const page = parseInt(searchParams.get("page")) || 1;
   const cat = searchParams.get("cat");
 
   const POST_PER_PAGE = 2;
@@ -18,23 +23,16 @@ export const GET = async (req) => {
     },
   };
 
-
-
-
-
-
-  
-  
   try {
     const [posts, count] = await prisma.$transaction([
       prisma.post.findMany(query),
       prisma.post.count({ where: query.where }),
     ]);
-    return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
+    return new NextResponse(JSON.stringify({ posts, count }), { status: 200 });
   } catch (err) {
     console.log(err);
     return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+      JSON.stringify({ message: "Something went wrong!" }), { status: 500 }
     );
   }
 };
