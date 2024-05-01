f14868ed3cec8913a5e43e9b89958a1e750048fa
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-01 16:46:13 +0545

    Update Firebase configuration in firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -5,13 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE,
-  authDomain: "blog-cddc2.firebaseapp.com",
-  projectId: "blog-cddc2",
-  storageBucket: "blog-cddc2.appspot.com",
-  messagingSenderId: "413696194974",
-  appId: "1:413696194974:web:bc0a551cd5558bdf0dbe8f"
+  apiKey: "AIzaSyAuFsC2OAm79-m-eYpWOeL0b2VOnc78S98",
+  authDomain: "techbichitra-6229f.firebaseapp.com",
+  projectId: "techbichitra-6229f",
+  storageBucket: "techbichitra-6229f.appspot.com",
+  messagingSenderId: "475555412295",
+  appId: "1:475555412295:web:9169902c8bfd25ab6d3462",
+  measurementId: "G-53V9G23MJ4"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
